3843e7fb4bfa9cb4c938fe6302a6ab55e0a9a1f2
Станислав Степаненко <ronin999@list.ru>
2025-11-19 14:29:29 +0300

    ДЗ27 new

--- config/settings.py
+++ config/settings.py
@@ -144,10 +144,17 @@ LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'catalog:home'
 LOGOUT_REDIRECT_URL = 'catalog:home'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'          # пример
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = os.getenv(
+    'EMAIL_BACKEND',
+    'django.core.mail.backends.smtp.EmailBackend'
+)
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
+
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.utils import InterfaceError
+from django.core.exceptions import ValidationError
 
 
 class User(AbstractUser):
@@ -33,11 +35,39 @@ class User(AbstractUser):
     )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []  # без обязательного username/first_name и т.п.
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def validate_unique(self, exclude=None):
+        """
+        Переопределяем validate_unique для обработки ошибки psycopg3.
+        """
+        try:
+            super().validate_unique(exclude=exclude)
+        except InterfaceError as e:
+            if 'row must be included between 0 and 0' in str(e):
+                errors = {}
+
+                if 'email' not in (exclude or []):
+                    if self.email:
+                        try:
+                            qs = User.objects.filter(email=self.email)
+                            if self.pk:
+                                qs = qs.exclude(pk=self.pk)
+                            # list(qs[:1]) вместо exists()
+                            if list(qs[:1]):
+                                errors['email'] = ['Пользователь с таким email уже существует.']
+                        except InterfaceError:
+                            # если и тут что-то упало — не душим пользователя
+                            pass
+
+                if errors:
+                    raise ValidationError(errors)
+            else:
+                raise
 
     def __str__(self):
         return self.email or f'User #{self.pk}'
